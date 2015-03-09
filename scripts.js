b93f074fa5547c255a092ffa84b2bcb871034f44
zymaislam <zyma.islam90@gmail.com>
2015-03-08 23:14:38 -0400

    broken code sill. changed all my var names to the default in leaflet

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
+
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
