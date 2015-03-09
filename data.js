935572440c0b267bb53b8ad917935a3b9fd29ecc
zymaislam <zyma.islam90@gmail.com>
2015-03-08 23:36:35 -0400

    I have no idea why my code is broken…

--- data.js
+++ data.js
@@ -1,13 +1,14 @@
  
-  var mapData ={
-  	
-  	"rows" : [
+  var mapData =
+  
+{
+  "rows" : [
     {
       "CenterName" : "Mainchance",
       "Borough" : "Manhattan",
       "Location" : "120 East 32nd Street\n",
       "Fulladdress" : "120 East 32nd Street\n, New York, NY",
-      "Longitude" : null,
+      "longitude" : -73.981548,
       "latitude" : 40.745305,
       "Comments" : "Open 24 hours"
     },
@@ -16,7 +17,7 @@
       "Borough" : "Bronx",
       "Location" : "1241 Lafayette Avenue\n",
       "Fulladdress" : "1241 Lafayette Avenue\n, New York, NY",
-      "Longitude" : null,
+      "longitude" : -73.99064,
       "latitude" : 40.730623,
       "Comments" : "Open 24 hours"
     },
@@ -25,7 +26,7 @@
       "Borough" : "Manhattan",
       "Location" : "257 West 30th Street\n",
       "Fulladdress" : "257 West 30th Street\n, New York, NY",
-      "Longitude" : null,
+      "longitude" : -73.993654,
       "latitude" : 40.749221,
       "Comments" : "Open from 7:30 a.m.-8:30 p.m. This program remain open 24 hours during winter months"
     },
@@ -34,7 +35,7 @@
       "Borough" : "Brooklyn",
       "Location" : "2402 Atlantic Avenue\n",
       "Fulladdress" : "2402 Atlantic Avenue\n, New York, NY",
-      "Longitude" : null,
+      "longitude" : -77.484901,
       "latitude" : 43.153541,
       "Comments" : "Open from 7:30 a.m.-8:30 p.m. This program remain open 24 hours during winter months"
     },
@@ -43,7 +44,7 @@
       "Borough" : "Staten Island",
       "Location" : "25 Central Avenue\n",
       "Fulladdress" : "25 Central Avenue\n, New York, NY",
-      "Longitude" : null,
+      "longitude" : -74.018335,
       "latitude" : 40.689792,
       "Comments" : "Open from 7:30 a.m.-8:30 p.m. This program remain open 24 hours during winter months"
     }
--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([40.745198, -73.981476], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var mapDataArray = mapData.rows;
+
+for (var i = 0; i < mapDataArray.length; i++) {
+	
+	var location = mapDataArray[i];
+	
+var marker = L.marker([location.latitude, location.longitude]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
