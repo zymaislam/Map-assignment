b0ae7719c075f250afe06efe5f3c40228daa3c39
zymaislam <zyma.islam90@gmail.com>
2015-03-08 20:55:33 -0400

    added a div with a solid black border and a tile layer to my map

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var map = L.map('homelessMap').setView([40.745198, -73.981476], 13);
+
+//this is my map tile
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var numShelter = warmShelterData.rows;
+
+for (var i = 0; i < numShelter.length; i++) {
+	var onewarmShelter = numShelter [i];
+	
+	
+
+	var marker = L.marker([onewarmShelter.Lat, onewarmShelter.Lng]).addTo(map);
+	marker.bindPopup("<b>" +onewarmShelter.FullAddress+"</br><br>"+onewarmShelter.ProgramName);
+};
