8abda22400f5bc89ef13bbb04e0d85a3b25a5848
zymaislam <zyma.islam90@gmail.com>
2015-03-08 23:04:03 -0400

    it can’t find leaflet.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,5 @@
-var map = L.map('homelessMap').setView([40.745198, -73.981476], 13);
-//this is setting my default coordinates to new york
-
-//this is my map tile
-
+var map = L.map('map').setView([51.505, -0.09], 13);
+// add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-var mapDataArray = mapData.rows;
-
-for (var i = 0; i < mapDataArray.length; i++) {
-	
-	var locationCenter = mapDataArray[i];
-	
-var marker = L.marker([locationCenter.latitude, locationCenter.longitude]).addTo(map);
-
-marker.bindPopup("<b>*Full address*</b>This is the homeless shelter in *borough*<br>").openPopup();
